d206550fa3eecc03b09b14b99db11571f2ef72f1
Muhammadbobur <bobur0218programmer@gmail.com>
2026-01-10 14:58:17 +0500

    feat: update home content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,32 +12,26 @@ export default function HomePage() {
         <h1 className="text-2xl xl:text-3xl">Hello! I am Muhammadbobur</h1>
         <span className="text-grey text-xs xl:text-sm flex items-center flex-wrap gap-1">
           <TbCode />
-          Software Engineer&nbsp; | &nbsp;
+          <span title="i suck at it">Software Engineer*&nbsp; | &nbsp;</span>
           <FaUserGraduate />
-          WIUT Student&nbsp; | &nbsp;
+          <span title="i hate it">WIUT Student&nbsp; | &nbsp;</span>
           <SiArchlinux />
-          I use Arch btw&nbsp; | &nbsp;
+          <span title="ai helped with my setup">I use Arch btw&nbsp; | &nbsp;</span>
           <TbDeviceGamepad2 />
-          Gamer
+          <span title="free fire #1982783322">Gamer</span>
         </span>
       </div>
 
       <div className="text-justify flex flex-col gap-4 xl:gap-8">
-        <p>
-          I am just average programmer. Nothing fancy. Never built something unique. Always, copied others ideas and used in my works. Im not cp genius or leetcode cracker. I love programming coz I love looking at editor themes: files icons, colors, nerd fonts... I love typing on my mechanical keyboard. Yea, this may sound stupid but while coding I love writing well formatted code ... always, without it I cant think of any other thing.
-        </p>
+        <p>Random weird guy, who counts himself as software engineer</p>
 
-        <p>
-          Currently, I am working at Ipak Yuli Bank, as frontend dev, since May, 2025. Thanks god for havin great friend who refered me to this job. I learned Vue there. But, I wont say that vue is better than react or reverse. I dont care abt prefering specific lang or framework. All of them are unique and beautiful in their own way.
-        </p>
+        <p>I know vue2 and use it at my <span title="iyb">work</span>. I guess I know JavaScript</p>
 
-        <p>
-          I love binge watching anime, movies and tv series. Though, I am not movie goer, never been in one. While coding I love listening musics, but I dont remember the names of the most songs. When I have free time, I love playing free fire and mlbb. I am average player there too.
-        </p>
+        <p>I have got tons of bullshit unfinshed projects in my github</p>
 
-        <p>
-          This is me. Thanks for reading this.
-        </p>
+        <p>I like playing free fire and binge watching</p>
+
+        <p>still tryin to discover who in the world i am</p>
       </div>
     </main>
   );
